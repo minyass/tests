db74e4aef8a7e867cda65b0e4b7459c3fdd96bad
Yasmin Machado <83288141+minyass@users.noreply.github.com>
2021-11-28 21:09:45 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import math
+print ("resolva a equação 3x² + 4x + 1 = 0")
+a = input("insira a:" )
+b = input("insira b:" )
+c = input("insira c:" )
+
+delta = int (b) ** 2 - 4 * int (a) * int (c)
+if delta > 0:
+ x_one = (- int (b) + math.sqrt(delta)) / 2 * 1
+ x_two = (- int (b) - math.sqrt(delta)) / 2 * 1
+ print (f"Delta = {delta}, S ={x_one},{(x_two)}")
+elif delta == 0:
+ x_one = (- int (b) + math.sqrt(delta)) / 2 * 1
+ x_two = (- int (b) - math.sqrt(delta)) / 2 * 1
+ print (f"Delta = {delta}, S ={x_one},{(x_two)}")
+elif delta < 0:
+  print (f"Delta = {delta}, S = null")
